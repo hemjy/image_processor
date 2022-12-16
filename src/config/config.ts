869dbb24d19211c441eb8aaf2a64f2803b87cb03
Hashim <hashimabdulmujib@gmail.com>
2022-12-16 22:08:02 +0100

    fix env variable issue

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,9 @@
+
+declare var process : {
+  env: {
+    JWT_SECRET: string
+  }
+}
 export const config = {
     "jwt": {
             "secret": process.env.JWT_SECRET
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,10 +5,9 @@ import { config } from '../config/config';
 
 const c = config.jwt;
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
-    console.warn(c.secret)
-  
+    
     if (!req.headers || !req.headers.authorization){
-        return res.status(401).send({ message: 'No authorization headers.'+ c.secret});
+        return res.status(401).send({ message: 'No authorization headers.'});
     }
     
 
